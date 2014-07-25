b2f5d7496c70a1290ca595d0d60c4a1eb90bf7af
Your Name <you@example.com>
2014-07-26 01:35:36 +0200

    fading alices

--- js/bridge-lazy-jquery.js
+++ js/bridge-lazy-jquery.js
@@ -1,14 +1,22 @@
 var bridge = function(jq) {
   return {
     'get': function(n) {
-      (n && n > 0 && n < jq.length) ? jq.eq(n) : undefined;
+      console.log('getting ' + n);
+      return jq.eq(n);
     },
     'length': function() {
-      return jq.length;
+      return jq.length + 2; // this +2 is here due to a bug 
     }
   };
 };
 
 $(document).ready(function() {
   var ll = List.Lazy(bridge($("li")));
+  ll.filter(function(jq) {
+    console.log('trying "' + jq.text() + "'");
+    return jq.text().indexOf("Alice") > -1;
+  }).each(function(jq) {
+    console.log(jq.text() + ' contains alice');
+    jq.addClass('faded');
+  });
 });
