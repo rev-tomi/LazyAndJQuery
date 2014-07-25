f3fb747d500381e01e0e75b0aa6f9e8bc54260fd
Your Name <you@example.com>
2014-07-26 01:13:23 +0200

    putting them together step 1

--- js/bridge-lazy-jquery.js
+++ js/bridge-lazy-jquery.js
@@ -1,10 +1,14 @@
 var bridge = function(jq) {
   return {
     'get': function(n) {
-      (n && n > 0 && n < jq.length()) ? jq.eq(n) : undefined;
+      (n && n > 0 && n < jq.length) ? jq.eq(n) : undefined;
     },
     'length': function() {
-      return jq.length();
+      return jq.length;
     }
   };
 };
+
+$(document).ready(function() {
+  var ll = List.Lazy(bridge($("li")));
+});
