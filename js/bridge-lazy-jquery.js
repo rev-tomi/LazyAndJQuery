99abecf555d2d2cacb7197a81b49fb4e615d636e
Your Name <you@example.com>
2014-07-27 14:07:18 +0200

    encapsulating code better

--- js/bridge-lazy-jquery.js
+++ js/bridge-lazy-jquery.js
@@ -1,14 +1,15 @@
-var LazyJQueryBridge = function(jq) {
-  return {
+List.Lazy.fromJQuery = function(jq) {
+  var adapter = {
     'get': function(n) {
       return jq.eq(n);
     },
     'length': jq.length
   };
+  return List.Lazy(adapter);
 };
 
 $(document).ready(function() {
-  var ll = List.Lazy(LazyJQueryBridge($("li")));
+  var ll = List.Lazy.fromJQuery($("li"));
   ll.filter(function(jq) {
     return jq.text().indexOf("Alice") > -1;
   }).each(function(jq) {
